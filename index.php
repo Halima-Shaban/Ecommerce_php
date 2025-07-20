4e0cd6f9ed43a7e50430d5f0b6ec1eab18972e91
Halima <h78001227@gmail.com>
2025-07-19 23:00:37 -0700

    update description

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php  include "header.php";?>
 <?php  include "footer.php";?>
 <h1>Hello from home page</h1>
-<p>main home page</p>
+<p>main home page Lorem ipsum dolor sit amet.</p>
 <?php
 
 $name = "halima";
